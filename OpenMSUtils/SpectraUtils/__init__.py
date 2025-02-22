e359c31a3b41c496ed573e0534d1d07f7c055fa6
YuchengLiao <ycliao@zju.edu.cn>
2025-02-22 20:10:51 +0800

    fix: add new class in __init__

--- OpenMSUtils/SpectraUtils/__init__.py
+++ OpenMSUtils/SpectraUtils/__init__.py
@@ -2,3 +2,5 @@ from .MSReader import MS1Reader, MS2Reader
 from .MSObject import MSObject
 from .MGFReader import MGFReader
 from .MZMLReader import MZMLReader
+from .SpectrumPlotter import SpectrumPlotter
+from .IonMobilityUtils import IonMobilityUtils
